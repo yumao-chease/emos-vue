71d0ed4b751e80ec0ff17b401a0b77a349ae77e5
tanjunyang <junyang.qin@outlook.com>
2022-01-11 14:25:37 +0800

    用户分页信息查询

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,15 +1,7 @@
 <template>
 	<div v-if="isAuth(['ROOT', 'USER:SELECT'])">
 		<el-form :inline="true" :model="dataForm" :rules="dataRule" ref="dataForm">
-			<el-form-item prop="name">
-				<el-input
-					v-model="dataForm.name"
-					placeholder="姓名"
-					size="medium"
-					class="input"
-					clearable="clearable"
-				/>
-			</el-form-item>
+			<el-form-item prop="name"><el-input v-model="dataForm.name" placeholder="姓名" size="medium" class="input" clearable="clearable" /></el-form-item>
 			<el-form-item>
 				<el-select v-model="dataForm.sex" class="input" placeholder="性别" size="medium" clearable="clearable">
 					<el-option label="男" value="男" />
@@ -22,58 +14,24 @@
 				</el-select>
 			</el-form-item>
 			<el-form-item>
-				<el-select
-					v-model="dataForm.deptId"
-					class="input"
-					placeholder="部门"
-					size="medium"
-					clearable="clearable"
-				>
+				<el-select v-model="dataForm.deptId" class="input" placeholder="部门" size="medium" clearable="clearable">
 					<el-option v-for="one in deptList" :label="one.deptName" :value="one.id" />
 				</el-select>
 			</el-form-item>
 			<el-form-item>
-				<el-select
-					v-model="dataForm.status"
-					class="input"
-					placeholder="状态"
-					size="medium"
-					clearable="clearable"
-				>
+				<el-select v-model="dataForm.status" class="input" placeholder="状态" size="medium" clearable="clearable">
 					<el-option label="在职" value="1" />
 					<el-option label="离职" value="2" />
 				</el-select>
 			</el-form-item>
 			<el-form-item>
 				<el-button size="medium" type="primary" @click="searchHandle()">查询</el-button>
-				<el-button
-					size="medium"
-					type="primary"
-					:disabled="!isAuth(['ROOT', 'USER:INSERT'])"
-					@click="addHandle()"
-				>
-					新增
-				</el-button>
-				<el-button
-					size="medium"
-					type="danger"
-					:disabled="!isAuth(['ROOT', 'USER:DELETE'])"
-					@click="deleteHandle()"
-				>
-					批量删除
-				</el-button>
+				<el-button size="medium" type="primary" :disabled="!isAuth(['ROOT', 'USER:INSERT'])" @click="addHandle()">新增</el-button>
+				<el-button size="medium" type="danger" :disabled="!isAuth(['ROOT', 'USER:DELETE'])" @click="deleteHandle()">批量删除</el-button>
 			</el-form-item>
 		</el-form>
-		<el-table
-			:data="dataList"
-			border
-			v-loading="dataListLoading"
-			@selection-change="selectionChangeHandle"
-			cell-style="padding: 4px 0"
-			style="width: 100%;"
-			size="medium"
-		>
-			<el-table-column type="selection" header-align="center" align="center" width="50"/>
+		<el-table :data="dataList" border v-loading="dataListLoading" @selection-change="selectionChangeHandle" cell-style="padding: 4px 0" style="width: 100%;" size="medium">
+			<el-table-column type="selection" header-align="center" align="center" width="50" />
 			<el-table-column type="index" header-align="center" align="center" width="100" label="序号">
 				<template #default="scope">
 					<span>{{ (pageIndex - 1) * pageSize + scope.$index + 1 }}</span>
@@ -82,35 +40,14 @@
 			<el-table-column prop="name" header-align="center" align="center" min-width="100" label="姓名" />
 			<el-table-column prop="sex" header-align="center" align="center" min-width="60" label="性别" />
 			<el-table-column prop="tel" header-align="center" align="center" min-width="130" label="电话" />
-			<el-table-column
-				prop="email"
-				header-align="center"
-				align="center"
-				min-width="240"
-				label="邮箱"
-				:show-overflow-tooltip="true"
-			/>
+			<el-table-column prop="email" header-align="center" align="center" min-width="240" label="邮箱" :show-overflow-tooltip="true" />
 			<el-table-column prop="hiredate" header-align="center" align="center" min-width="130" label="入职日期" />
-			<el-table-column
-				prop="roles"
-				header-align="center"
-				align="center"
-				min-width="150"
-				label="角色"
-				:show-overflow-tooltip="true"
-			/>
+			<el-table-column prop="roles" header-align="center" align="center" min-width="150" label="角色" :show-overflow-tooltip="true" />
 			<el-table-column prop="dept" header-align="center" align="center" min-width="120" label="部门" />
 			<el-table-column prop="status" header-align="center" align="center" min-width="100" label="状态" />
 			<el-table-column header-align="center" align="center" width="150" label="操作">
 				<template #default="scope">
-					<el-button
-						type="text"
-						size="medium"
-						v-if="isAuth(['ROOT', 'USER:UPDATE'])"
-						@click="updateHandle(scope.row.id)"
-					>
-						修改
-					</el-button>
+					<el-button type="text" size="medium" v-if="isAuth(['ROOT', 'USER:UPDATE'])" @click="updateHandle(scope.row.id)">修改</el-button>
 					<el-button
 						type="text"
 						size="medium"
@@ -120,15 +57,7 @@
 					>
 						离职
 					</el-button>
-					<el-button
-						type="text"
-						size="medium"
-						:disabled="scope.row.root"
-						v-if="isAuth(['ROOT', 'USER:DELETE'])"
-						@click="deleteHandle(scope.row.id)"
-					>
-						删除
-					</el-button>
+					<el-button type="text" size="medium" :disabled="scope.row.root" v-if="isAuth(['ROOT', 'USER:DELETE'])" @click="deleteHandle(scope.row.id)">删除</el-button>
 				</template>
 			</el-table-column>
 		</el-table>
@@ -179,6 +108,64 @@ export default {
 		};
 	},
 	methods: {
+		sizeChangeHandle(val) {
+		    this.pageSize = val;
+		    //更改每页显示记录数量后，都从第一页开始查询
+		    this.pageIndex = 1;
+		    this.loadDataList();
+		},
+		currentChangeHandle(val) {
+		    this.pageIndex = val;
+		    this.loadDataList();
+		},
+		searchHandle: function() {
+		    //先执行表单验证
+		    this.$refs['dataForm'].validate(valid => {
+		        if (valid) {
+		            //清理页面上的表单验证结果
+		            this.$refs['dataForm'].clearValidate();
+		            //不允许上传空字符串给后端，但是可以传null值
+		            if (this.dataForm.name == '') {
+		                this.dataForm.name = null;
+		            }
+		            //如果当前页面不是第一页，则跳转到第一页显示查询的结果
+		            if (this.pageIndex != 1) {
+		                this.pageIndex = 1;
+		            }
+		            this.loadDataList();
+		        } else {
+		            return false;
+		        }
+		    });
+		},
+		loadDataList: function() {
+		    let that = this;
+		    //请求后端数据的时候，让表格出现循环滚动的等待图标
+		    that.dataListLoading = true;
+		    let data = {
+		        pageIndex: that.pageIndex,
+		        pageSize: that.pageSize,
+		        name: that.dataForm.name,
+		        sex: that.dataForm.sex,
+		        role: that.dataForm.role,
+		        deptId: that.dataForm.deptId,
+		        status: that.dataForm.status
+		    };
+		    that.$http('user/searchUserByPage', 'POST', data, true, function(resp) {
+		        let page = resp.page;
+		        let list = page.list;
+		        for (let one of list) {
+		            if (one.status == 1) {
+		                one.status = '在职';
+		            } else if (one.status == 2) {
+		                one.status = '离职';
+		            }
+		        }
+		        that.dataList = list;
+		        that.totalCount = page.totalCount;
+		        that.dataListLoading = false;
+		    });
+		},
 		loadRoleList: function() {
 			let that = this;
 			that.$http('role/searchAllRole', 'GET', null, true, function(resp) {
@@ -196,6 +183,7 @@ export default {
 		}
 	},
 	created: function() {
+		this.loadDataList();
 		this.loadRoleList();
 		this.loadDeptList();
 	}
